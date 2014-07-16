9f680c2e44feb9c5dad4afd7b7228a5bc79d726c
Barry Hoggard <barry@paddle8.com>
2014-07-16 18:44:55 -0400

    custom command to tweet openings WIP

---
+++ artcalendar/management/commands/tweet.py
@@ -0,0 +1,19 @@
+from django.core.management.base import BaseCommand, CommandError
+from artcalendar.models import Event
+import os
+from buffpy.api import API
+from buffpy.managers.profiles import Profiles
+
+
+class Command(BaseCommand):
+    help = "Send today's openings to https://bufferapp.com/"
+
+    def handle(self, *args, **options):
+        CLIENT_ID = os.environ.get('BUFFER_CLIENT_ID')
+        CLIENT_SECRET = os.environ.get('BUFFER_CLIENT_SECRET')
+        ACCESS_TOKEN = os.environ.get('BUFFER_ACCESS_TOKEN')
+
+        api = API(client_id=CLIENT_ID, client_secret=CLIENT_SECRET, access_token=ACCESS_TOKEN)
+        profile = Profiles(api=api).filter(service='twitter')[0]
+
+        # profile.updates.new("Testing new auto-tweeter for openings")
