d90371d8502db4ba32313dbce21324e434f22826
Barry Hoggard <barry@tristanmedia.com>
2014-07-20 14:48:17 -0400

    Revert "change from heroku instructions for static assets"
    This reverts commit d673472aa309fc340307b7fd285527d41767a73b.

--- filterizer/settings/base.py
+++ filterizer/settings/base.py
@@ -87,10 +87,10 @@ ALLOWED_HOSTS = ['*']
 
 # Static asset configuration
 import os
-PROJECT_PATH = os.path.dirname(os.path.abspath(__file__))
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
-    os.path.join(PROJECT_PATH, 'static'),
+    os.path.join(BASE_DIR, 'static'),
 )
